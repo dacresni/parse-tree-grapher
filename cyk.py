d4fbf485cf170182529c9e579b1aa2c1dc7207ba
Nehemiah I. Dacres <dacresni@slu.edu>
2010-04-20 12:45:33 -0500

    thought i fixed that thing

--- cyk.py
+++ cyk.py
@@ -6,22 +6,23 @@ from bnflexar import BnfLexar
 
 class CYKChart(object):
     def __init__(self):
-        self.chart = []
+        self.chart =[]
     def Build_CYK_Chart (self, aGrammar,aString, ) : # a 
         n = len(aString)
-        chart =self.chart # for n=5 thats [[][][][][]]
+        self.chart= [[None]*n]*n
+        chart= self.chart
         print "aString",aString
         print "aGrammar",aGrammar
-        for i in aString :
-            chart.append([ aGrammar.shortMatch(i) ]) # append a list with the result of shortMatch(i) if no match, shortMatch should return None
+        for i in range(n):
+            chart[i][0]= aGrammar.shortMatch(aString[i])  # append a list with the result of shortMatch(i) if no match, shortMatch should return None
         print "chart",chart
         for j in range(1,n+1): #range drops the endpoint
             for i in range((n-j+1)):  # step 4 in pg 140 of Hopcroft and range is (first, last-1)
                 print i,j
-                chart[i].append([]) #append the empty set
+                #chart[i].append([]) #append the empty set
                 for k in range(j):
-                    if not chart[i][j]:
-                        chart[i][j].append(aGrammar.longMatch(chart[i][k],chart[i+k][j-k]))
+                    if chart[i][j] == None:
+                        chart[i][j]=aGrammar.longMatch(chart[i][k],chart[i+k][j-k])
                     #if nothings there, try the chart it MUST be in that order 
                 #done
             #end for i
