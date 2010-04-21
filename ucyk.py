afb3903f887e9e2e29b3036d1f9258338c333afa
Nehemiah I. Dacres <dacresni@slu.edu>
2010-04-21 08:52:13 -0500

    moved comments and renamed cykNOASCII

--- ucyk.py
+++ ucyk.py
@@ -2,26 +2,32 @@
 import sys
 from lexar import Token
 from grammar import Grammar
+
 class CYKChart(object):
     def __init__(self):
-        self.chart = []
-    def Build_CYK_Chart (self, aGrammar,aString, chart=self.chart) : # a 
+        self.chart =[]
+    def Build_CYK_Chart (self, aGrammar,aString, ) : # a 
         n = len(aString)
-        chart = []# for n=5 thats [[][][][][]]
-        for i in aString :
-            chart.append([ aGrammar.shortMatch(i) ]) # append a list with the result of shortMatch(i) if no match, shortMatch should return None
-        for j in  range(1,n+1): #range drops the endpoint
-            for i in range(0,(n-j+1)+1):  # step 4 in pg 140 of Hopcroft and range is (first, last-1)
-                chart[i].append([]) #append the empty set
-                for k in range(0,j):
-                    chart[i][j].append(chart[i][j] or aGrammar.shortMatch(chart[i][k],chart[i+k][j-k])) #if nothings there, try the chart it MUST be in that order 
+        self.chart= [[None]*n]*n
+        chart= self.chart
+        print "aString",aString
+        print "aGrammar",aGrammar
+        for i in range(n):
+            chart[i][0]= aGrammar.shortMatch(aString[i])  # append a list with the result of shortMatch(i) if no match, shortMatch should return None
+        print "chart",chart
+        for j in range(1,n): #range drops the endpoint
+            for i in range(n-j+1):  # step 4 in pg 140 of Hopcroft and range is (first, last-1)
+                print i,j
+                #chart[i].append([]) #append the empty set
+                for k in range(j):
+                    if chart[i][j] == None:
+                        chart[i][j]=aGrammar.longMatch(chart[i][k],chart[i+k][j-k])
+                    #if nothings there, try the chart it MUST be in that order 
                 #done
             #end for i
         #end for j
     #end def
-    def __repr__(self):
-        return self.chart
-""" 
+"""
 Let Close(X) = { B | B →* A, using unary productions, and A ϵ X}
 Build CYK Chart(t,[w1 ,. . . ,wn ])
     for j ← 1 to n do
