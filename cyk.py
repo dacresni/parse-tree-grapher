1355237e33a605dfa4245ce6f7ddc91fff581460
Nehemiah I Dacres <vivacarlie@gmail.com>
2010-05-01 12:13:22 -0500

    not sure

--- cyk.py
+++ cyk.py
@@ -9,22 +9,22 @@ class CYKChart(object):
         self.chart =[]
         #self.chart= [ [set() for _ in range(n)] for _ in range(n)]
     def Build_CYK_Chart (self, aGrammar,aString ) : # a 
-        n = len(aString)+1
+        n = len(aString)
         self.chart= [ [set() for _ in range(n)] for _ in range(n)]
         print "in Build_CYK_Chart"
         print "aString",aString
         print "aGrammar",aGrammar
         for i in range(1,n):
             print "i=",i
-            print "DEBUG: chart ",i,aString[i-1],aGrammar.singleMatch(aString[i-1]) 
-            self.chart[i][1]=aGrammar.singleMatch(aString[i-1])
+            print "DEBUG: chart ",i,aString[i],aGrammar.singleMatch(aString[i]) 
+            self.chart[i][1]=aGrammar.singleMatch(aString[i])
             print "After setting...",self.chart[i][1] 
         print "chart after len 1 dealt with\n",self
-        for j in range(2,n): #range drops the endpoint
-            for i in range(1,n-j+1):  # step 4 in pg 140 of Hopcroft and range is (first, last-1)
-                for k in range(1,j-1):
-                   print i,j,":=",i,k,",",self.chart[i][k],self.chart[i+k][j-k]
-                   matches=aGrammar.setMatchLong(self.chart[i][k],self.chart[i+k][j-k])
+        for j in range(1,n): #range drops the endpoint
+            for i in range(n-j+1):  # step 4 in pg 140 of Hopcroft and range is (first, last-1)
+                for k in range(j-1):
+                   print i,j,":=",i,k,",",self.chart[i][k],self.chart[i+k+1][j-k-1]
+                   matches=aGrammar.setMatchLong(self.chart[i][k],self.chart[i+k+1][j-k-1])
                    self.chart[i][j].update(matches)
         #print self
                     #if nothings there, try the chart it MUST be in that order 
