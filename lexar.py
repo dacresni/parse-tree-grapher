dccc090284b3dd11488eca4bc06c5115a420d4e5
Nehemiah I Dacres <vivacarlie@gmail.com>
2010-05-03 22:10:08 -0500

    change print spacing

--- lexar.py
+++ lexar.py
@@ -6,7 +6,7 @@ class Token(object):
         self.type=type #terminals should really be classifyable
         self.value=value
     def __str__(self):
-        return "(%s , %s )"%(self.type,self.value)
+        return "( %s , %s )"%(self.type,self.value)
     def __repr__(self):
         return self.value
 
