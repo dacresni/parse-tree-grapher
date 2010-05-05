45c615172f98d2ca116d0846ac2839ddcb65f911
Nehemiah I. Dacres <dacresni@slu.edu>
2010-05-05 11:38:01 -0500

    changed graph algorihm to properly label graphs

--- viz.py
+++ viz.py
@@ -6,8 +6,8 @@ class genDot(object):
             visited.add(here)
             if neighbors.has_key(here):  # if it has neighbors
                 for next in neighbors[here]:
-                    self.edges.add((here,next))
-                    self.specFile+='  "%s" -> "%s" ;\n'%(here,next )
+                    self.edges.append((here,next))
+                    #self.specFile+='  "%s" -> "%s" ;\n'%(here,next )
                     self.genTree(G,next,visited)
     #if your not connected, you wont be in the bfs        
     def __init__(self,C,filename=None):
@@ -16,16 +16,16 @@ class genDot(object):
         else:
             self.filename=filename
         self.specFile=""
-        self.edges=set()
+        self.edges=[]
         self.visited = set()
         print C.graph
         if not C.graph.has_key((1,len(C)-1)):
             raise Exception("parse error")
         self.genTree(C.graph, (1,len(C)-1),self.visited )
-        #for edge in self.edges:
-        #    to = edge[0]
-        #    fro = edge[1]
-        #    self.specFile+='  "%s" -> "%s" ;\n'%(to,fro )
+        for edge in self.edges:
+            to = edge[0]
+            fro = edge[1]
+            self.specFile+='  "%s" -> "%s" ;\n'%(to,fro )
         for node in self.visited :
             lable=C.chart[node[0]][node[1]]
             self.specFile+='    "%s"[label = "%s"] ;\n'%(node,lable ) #find lable for leaf node 
