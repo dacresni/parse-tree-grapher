3457a40b54ad40e44ddb92006702e6e38fa3462b
Nehemiah I. Dacres <dacresni@slu.edu>
2010-05-07 15:19:10 -0500

    experiment to remove dup edges unsucessfull

--- viz.py
+++ viz.py
@@ -7,7 +7,7 @@ class genDot(object):
             if neighbors.has_key(here):  # if it has neighbors
                 for next in neighbors[here]:
                     self.edges.append((here,next))
-                    #self.specFile+='  "%s" -> "%s" ;\n'%(here,next )
+                    self.specFile+='  "%s" -> "%s" ;\n'%(here,next )
                     self.genTree(G,next,visited)
     #if your not connected, you wont be in the bfs        
     def __init__(self,C,filename=None):
@@ -22,10 +22,10 @@ class genDot(object):
         if not C.graph.has_key((1,len(C)-1)):
             raise Exception("parse error")
         self.genTree(C.graph, (1,len(C)-1),self.visited )
-        for edge in self.edges:
-            to = edge[0]
-            fro = edge[1]
-            self.specFile+='  "%s" -> "%s" ;\n'%(to,fro )
+        #for edge in self.edges:
+        #    to = edge[0]
+        #    fro = edge[1]
+        #    self.specFile+='  "%s" -> "%s" ;\n'%(to,fro )
         for node in self.visited :
             lable=C.chart[node[0]][node[1]]
             self.specFile+='    "%s"[label = "%s"] ;\n'%(node,lable ) #find lable for leaf node 
