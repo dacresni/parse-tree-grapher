9e818bbef4a7a99a1de3c71654009436a9138da5
Nehemiah I. Dacres <dacresni@slu.edu>
2010-05-07 15:27:28 -0500

    now the labels don't look say set anymore

--- viz.py
+++ viz.py
@@ -7,7 +7,7 @@ class genDot(object):
             if neighbors.has_key(here):  # if it has neighbors
                 for next in neighbors[here]:
                     self.edges.append((here,next))
-                    self.specFile+='  "%s" -> "%s" ;\n'%(here,next )
+                    self.specFile+='  "%s" -> "%s" ;\n'%((here,next ))
                     self.genTree(G,next,visited)
     #if your not connected, you wont be in the bfs        
     def __init__(self,C,filename=None):
@@ -28,7 +28,7 @@ class genDot(object):
         #    self.specFile+='  "%s" -> "%s" ;\n'%(to,fro )
         for node in self.visited :
             lable=C.chart[node[0]][node[1]]
-            self.specFile+='    "%s"[label = "%s"] ;\n'%(node,lable ) #find lable for leaf node 
+            self.specFile+='    "%s"[label = "%s"] ;\n'%(node,list(lable )) #find lable for leaf node 
 
         finalFile=open(self.filename,'w')
         finalFile.write("digraph G { \n")
