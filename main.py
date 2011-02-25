bebfa0aa7c5537964b5f173dacb7d2651f5d55cd
Nehemiah I Dacres <vivacarlie@gmail.com>
2011-02-25 12:23:18 -0600

    added a few helpfull comments

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ def main(args**):
     C.Build_CYK_Chart(G,S)
     print C
     print C.graph
-    genDot(C,outputfile)
+    genDot(C,outputfile) #here is where the dotfile is created
     system("dot -Tjpg %s  -o %s "%(outputfile, outputfile))
     print "%s ted"%(outputfile)
 
 if __name__ == '__main__':
     import sys
+    if len(sys.argv) < 2 :
+        print "usage: main.py lexfilename grammarspec inputfile \n returns graph of the parsing of the inputfile by the grammar and lexer "
     main(sys.argv)
     
